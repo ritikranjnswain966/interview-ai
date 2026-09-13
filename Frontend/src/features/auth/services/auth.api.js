4d47c3a49e5418a91b1b7b7ef79345ef1a2d346d
Ritik Ranjn Swain <ritikranjnswain966@gamil.com>
2026-09-13 10:21:54 +0530

    update API base URL to production server

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-ai-h3i4.onrender.com",
     withCredentials: true
 })
 
