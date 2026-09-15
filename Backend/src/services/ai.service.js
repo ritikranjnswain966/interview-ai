93ab09466df2cf28b31029b289665159957e2e30
Ritik Ranjn Swain <ritikranjnswain966@gamil.com>
2026-09-15 08:52:48 +0530

    refactor: update Puppeteer integration to use puppeteer-core and @sparticuz/chromium

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,7 +1,8 @@
 const { GoogleGenAI } = require("@google/genai");
 const { z } = require("zod");
 const { zodToJsonSchema } = require("zod-to-json-schema");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium")
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GENAI_API_KEY,
@@ -118,8 +119,9 @@ async function generateInterviewReport({
 
 async function generatePdfFromHtml(htmlContent) {
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+    executablePath: await chromium.executablePath(),
+    args: chromium.args,
+    headless: chromium.headless,
   });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
